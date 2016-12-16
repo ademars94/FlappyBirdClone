ca2d51956310facdae9e4fe5dc92544c28ee33d7
Alex DeMars <alexdemars@gmail.com>
2016-12-16 15:57:27 -0800

    add bird and make bird flap

---
+++ Bird.swift
@@ -0,0 +1,37 @@
+//
+//  Bird.swift
+//  FlappyBirdClone
+//
+//  Created by Alex DeMars on 12/16/16.
+//  Copyright © 2016 Alex DeMars. All rights reserved.
+//
+
+import SpriteKit
+
+struct CollisionType {
+  static let bird: UInt32 = 1
+  static let ground: UInt32 = 2
+  static let pipes: UInt32 = 3
+  static let score: UInt32 = 4
+}
+
+class Bird: SKSpriteNode {
+  
+  func initialize() {
+    // TODO: Create Animations
+    self.name = "Bird"
+    self.zPosition = 3
+    self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+    self.physicsBody = SKPhysicsBody(circleOfRadius: self.size.height / 2)
+    self.physicsBody?.affectedByGravity = true
+    self.physicsBody?.categoryBitMask = CollisionType.bird
+    self.physicsBody?.collisionBitMask = CollisionType.ground | CollisionType.pipes
+    self.physicsBody?.contactTestBitMask = CollisionType.ground | CollisionType.pipes | CollisionType.score
+  }
+  
+  func flap() {
+    self.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+    self.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 120))
+  }
+  
+}
--- GameplayScene.swift
+++ GameplayScene.swift
@@ -9,6 +9,8 @@
 import SpriteKit
 
 class GameplayScene: SKScene {
+  
+  var bird = Bird()
 
   override func didMove(to view: SKView) {
     print("Initializing...")
@@ -18,6 +20,23 @@ class GameplayScene: SKScene {
   func initialize() {
     createBackground()
     createGround()
+    createBird()
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    bird.flap()
+  }
+  
+  func createBird() {
+    bird = Bird(imageNamed: "Blue 1")
+    bird.initialize()
+    bird.position = CGPoint(x: -50, y: 0)
+    self.addChild(bird)
+  }
+  
+  override func update(_ currentTime: TimeInterval) {
+    moveBackground()
+    moveGround()
   }
   
   func createBackground() {
@@ -37,8 +56,32 @@ class GameplayScene: SKScene {
       ground.zPosition = 4
       ground.anchorPoint = CGPoint(x: 0.5, y: 0.5)
       ground.position = CGPoint(x: CGFloat(i) * ground.size.width, y: -(self.frame.size.height / 2))
+      ground.physicsBody = SKPhysicsBody(rectangleOf: ground.size)
+      ground.physicsBody?.affectedByGravity = false
+      ground.physicsBody?.isDynamic = false
+      ground.physicsBody?.categoryBitMask = CollisionType.ground
+      ground.physicsBody?.collisionBitMask = CollisionType.bird
+      ground.physicsBody?.contactTestBitMask = CollisionType.bird
       self.addChild(ground)
     }
+  }
+  
+  func moveBackground() {
+    enumerateChildNodes(withName: "BG", using: { node, error in
+      node.position.x -= 1
+      if node.position.x < -(self.frame.width) {
+        node.position.x += self.frame.width * 3
+      }
+    })
+  }
+  
+  func moveGround() {
+    enumerateChildNodes(withName: "Ground", using: { node, error in
+      node.position.x -= 3
+      if node.position.x < -(self.frame.width) {
+        node.position.x += self.frame.width * 3
+      }
+    })
   }
   
 }
